f735d50cabc3ebdbe6f4b9a6eb4ab4c72459df3f
ydah <13041216+ydah@users.noreply.github.com>
2023-02-25 17:18:08 +0900

    Rename Source to ParsedSource

--- lib/gashadokuro.rb
+++ lib/gashadokuro.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "gashadokuro/refinements"
-require_relative "gashadokuro/source"
+
+require_relative "gashadokuro/parsed_source"
 require_relative "gashadokuro/parser"
 require_relative "gashadokuro/version"
--- lib/gashadokuro/parsed_source.rb
+++ lib/gashadokuro/parsed_source.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Gashadokuro
-  class Source
+  class ParsedSource
     def initialize(source)
       source.force_encoding(Encoding::UTF_8) unless source.encoding == Encoding::UTF_8
 
